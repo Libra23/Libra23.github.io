688208a2142e010dab9d970cf72719aaf4afa1d2
Libra23 <kaya-libra-23@hotmail.co.jp>
2022-05-15 15:05:03 +0900

    reduce image size

--- deploy_by_sftp.sh
+++ deploy_by_sftp.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+rm -r out
+
 npm run build
 npm run export
 
 expect -c "
-set timeout 10
+set timeout 100
 spawn sftp ${SERVER_NAME}
+expect \"sftp>\"
+send \"rm ${DOMAIN_NAME}/public_html/* \n\"
 expect \"sftp>\"
 send \"put -r out/* ${DOMAIN_NAME}/public_html/ \n\"
 expect \"sftp>\"
--- pages/work.tsx
+++ pages/work.tsx
@@ -4,9 +4,11 @@ import { Box, Card, CardActionArea, CardMedia, CardContent, Divider, Grid, Typog
 import styles from '../styles/Work.module.css'
 
 const item_list = [{url:'https://miauchi.com/art-works/scratching-flippers-1/', 
-                    img:'/img/work/scratching_flippers_1.jpg', title:'Scratching Flippers Ⅰ', text:'was designed by modern artist, Yuri Miauchi. Several motors control flippers motion by use of a microcomputer. Max/MSP enables to collaborate between motion and music.'},
-                    {url:'/work/', 
-                    img:'/img/work/profile_movie.png', title:'ProfileMovie', text:'was presented to my family. It is first time to make video.'},]
+                    img:'/img/work/scratching_flippers_1.jpg', title:'Scratching Flippers I', text:'was designed by modern artist, Yuri Miauchi. Several motors control flippers motion by use of a microcomputer. Max/MSP enables to collaborate between motion and music.'},
+                   {url:'/work/', 
+                    img:'/img/work/profile_movie.png', title:'Profile Movie', text:'was presented to my family. It is first time to make video.'},
+                   {url:'/work/', 
+                    img:'/img/work/leather_wallet.jpg', title:'Leather Wallet', text:'was made of some leathers cutted by a laser cutter named Fabool Laser Mini.'},]
 
 const Work: NextPage = () => {
   return (
