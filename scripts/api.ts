2a5add48adf9fb635ac2a2d41fb42a9755c347f0
Libra <libra@MacBook-Air.local>
2023-08-20 11:35:56 +0900

    Revert "add appbarwithtab"
    This reverts commit 9482e8c79ccb20533d41936b5ee97971ca724d85.

--- scripts/api.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import { join } from 'path';
-import matter from 'gray-matter';
-
-export function getFilePath(dir: string): string[] {
-  const dir_path: string = join(process.cwd(), dir)
-  return fs.readdirSync(dir_path).map((file: string)=>join(dir_path, file))
-}
